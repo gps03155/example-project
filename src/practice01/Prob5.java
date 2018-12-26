249528eed91f2d49d2af289af8d2b476530edff7
박성혜 <gps03155@gmail.com>
2018-12-26 18:59:35 +0900

    Problem1 문제풀이

--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -3,5 +3,30 @@ package practice01;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		for(int i=0;i<100;i++) {
+			String num = String.valueOf(i);
+			boolean isCheck = false;
+			
+			for(int j=0;j<num.length();j++) {
+				if(num.charAt(j) == '3' || num.charAt(j) == '6' || num.charAt(j) == '9') {
+					System.out.print("짝");
+					isCheck = true;
+				}
+			}
+			
+			if(isCheck) {
+				System.out.println(num);
+			}
+			else {
+				continue;
+			}
+			
+			if(i % 10 == 0) {
+				System.out.println();
+			}
+			else {
+				System.out.print(' ');
+			}
+		}
 	}
 }
