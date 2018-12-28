1a57a745356fc9b47651102206356be9642aafab
박성혜 <gps03155@gmail.com>
2018-12-28 16:58:46 +0900

    TV 클래스, ArrayUtil 클래스 예제 과제 완료

--- src/chapter02/ArrayUtil.java
+++ src/chapter02/ArrayUtil.java
@@ -3,17 +3,34 @@ package chapter02;
 public class ArrayUtil {
 	// int 배열을 double 배열로 변환
 	public static double[] intToDouble( int[] source ) {
-		return null;
+		double[] result = new double[source.length];
+		
+		for(int i=0;i<source.length;i++) {
+			result[i] = source[i];
+		}
+		
+		return result;
 	}
 	
 	// double 배열을 int 배열로 변환
 	public static int[] doubleToInt( double[] source ) {
-		return null;
+		int[] result = new int[source.length];
+		
+		for(int i=0;i<source.length;i++) {
+			result[i] = (int) source[i];
+		}
+		
+		return result;
 	}
 	
 	// int 배열 두 개를 연결한 새로운 배열 리턴
 	public static int[] concat( int[] s1, int[] s2 ) {
-		return null;
+		int[] result = new int[s1.length + s2.length];
+
+		System.arraycopy(s1, 0, result, 0, s1.length);
+		System.arraycopy(s2, 0, result, 0, s2.length);
+
+		return result;
 	}
 
 }
--- src/chapter02/TV.java
+++ src/chapter02/TV.java
@@ -2,22 +2,101 @@ package chapter02;
 
 public class TV {
 	private boolean power;
-	private int channel;  //0~50
-	private int volume;   //0~50
+	private int channel; // 0~50
+	private int volume; // 0~50
 	
-	public void power(boolean on) {
+	public TV() {}
+	
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
 	}
 	
+	public boolean isPower() {
+		return power;
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
 	public void channel(int channel) {
+		channel = channelCheck(channel);
+		this.channel = channel;
+	}
+	
+	public void volume(int volume) {
+		volume = volumeCheck(volume);
+		this.volume = volume;
 	}
 	
 	public void channel(boolean up) {
+		if(!power) {
+			return;
+		}
+		
+		if(up) {
+			channel++;
+		}
+		else {
+			channel--;
+		}
+		
+		channel = channelCheck(channel);
+	}
+	
+	public void volume(boolean up) {
+		if(!power) {
+			return;
+		}
+		
+		if(up) {
+			volume++;
+		}
+		else {
+			volume--;
+		}
+		
+		volume = volumeCheck(volume);
+	}
+	
+	public void power(boolean on) {
+		if(on) {
+			power = true;
+		}
+		else {
+			power = false;
+		}
 	}
 	
 	public void status() {
-		System.out.println(
-			"TV[power="  + power   + 
-			", channel=" + channel + 
-			", volume="  + volume  + "]");
+		System.out.println("TV[power= " + power + ", channel= " + channel + ", volume= " + volume + "]");
+	}
+	
+	public int volumeCheck(int volume) {
+		if(volume > 50) {
+			volume = 0;
+		}
+		else if(volume < 0) {
+			volume = 50;
+		}
+		
+		return volume;
+	}
+	
+	public int channelCheck(int channel) {
+		if(channel > 50) {
+			channel = 0;
+		}
+		else if(channel < 0) {
+			channel = 50;
+		}
+		
+		return channel;
 	}
 }
