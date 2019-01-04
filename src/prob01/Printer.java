358aa51f427829c531d40e3cb53df660bd4f9637
박성혜 <gps03155@gmail.com>
2019-01-04 09:23:38 +0900

    Generic Method, muttable variable

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -1,19 +1,45 @@
 package prob01;
 
 public class Printer {
-	public void println(int num) {
-		System.out.println(num);
+//	public void println(int num) {
+//		System.out.println(num);
+//	}
+//	
+//	public void println(boolean visible) {
+//		System.out.println(visible);
+//	}
+//	
+//	public void println(double num) {
+//		System.out.println(num);
+//	}
+//	
+//	public void println(String name) {
+//		System.out.println(name);
+//	}
+//	
+	// Generic Method
+	// 타입에 관해서만 오버로드 대체 가능 - 개수 똑같을 때
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
-	
-	public void println(boolean visible) {
-		System.out.println(visible);
+
+	// 가변 변수
+	public Integer sum(Integer... nums) { // 인수를 여러개 쓸 수 있지만 배열로 넘김
+		Integer sum = 0;
+
+		for (Integer i : nums) {
+			sum += i;
+		}
+
+		return sum;
 	}
-	
-	public void println(double num) {
-		System.out.println(num);
-	}
-	
-	public void println(String name) {
-		System.out.println(name);
+
+	// Generic + 가변변수
+	// 오버로드를 완벽히 대체 - 매개변수, 개수 달라도 실행가능
+	@SuppressWarnings("unchecked")
+	public <T> void println(T... ts) {
+		for (T t : ts) {
+			System.out.println(t);
+		}
 	}
 }
