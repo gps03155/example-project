89ff698bde610b2f6fde2e3a31afbb471101b8cd
박성혜 <gps03155@gmail.com>
2019-01-15 15:46:33 +0900

    집계함수 add

--- 집계함수.sql
+++ 집계함수.sql
@@ -21,3 +21,21 @@ from (select max(salary) as max_salary, min(salary) as min_salary
 select salary, from_date
 from salaries
 where emp_no = 10060;
+
+
+-- 2번 직책 이동 --
+select count(*)
+from titles
+where emp_no = 10060;
+
+
+-- 직책별 이동 --
+select emp_no, count(*)
+from titles
+group by emp_no;
+
+
+select emp_no, avg(salary)
+from salaries
+group by emp_no
+having avg(salary) > 10000;
