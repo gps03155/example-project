85c75f9dcb0fa685d1bdd224b0b80f1a5aa511fa
박성혜 <gps03155@gmail.com>
2019-01-18 15:26:46 +0900

    실습문제4 - BookDao 풀이 완료

--- src/com/douzon/jdbc/bookshop/MainApp.java
+++ src/com/douzon/jdbc/bookshop/MainApp.java
@@ -1,8 +1,10 @@
 package com.douzon.jdbc.bookshop;
 
+import java.util.List;
 import java.util.Scanner;
 
 import com.douzon.jdbc.bookshop.dao.BookDao;
+import com.douzon.jdbc.bookshop.vo.BookVo;
 
 public class MainApp {
 
@@ -25,11 +27,20 @@ public class MainApp {
 		System.out.println("***** 도서 정보 출력하기 *****");
 		
 		// DB에서 출력
+		List<BookVo> bookVo = new BookDao().getList();
+		
+		for(BookVo vo : bookVo) {
+			System.out.println(vo.getNo() + " " + vo.getTitle() + " " + vo.getStatus() + " " + vo.getAuthorName());
+		}
 	}
 	
 	private static void rent(long no) {
 		// 책번호 입력 후 내용 갱신
-		new BookDao.updateStatus(no, "대여중");
+		BookDao bookDao = new BookDao();
+		boolean result = bookDao.updateStatus(no, "대여중");
 		
+		if(result) {
+			System.out.println("정보가 갱신되었습니다.");
+		}
 	}
 }
--- src/com/douzon/jdbc/bookshop/dao/BookDao.java
+++ src/com/douzon/jdbc/bookshop/dao/BookDao.java
@@ -18,52 +18,52 @@ public class BookDao {
 	ResultSet rs;
 	PreparedStatement pstmt;
 
-	public List<BookVo> getList(){
+	public List<BookVo> getList() {
 		List<BookVo> list = new ArrayList<BookVo>();
-		
+
 		try {
 			conn = getConnection();
-			
+
 			// 3. Statement 객체를 생성
 			stmt = conn.createStatement();
 
 			// 4. SQL문 실행
 			String sql = "select book.no, book.title, book.status, author.name from book join author on book.author_no = author.no order by book.no";
 			rs = stmt.executeQuery(sql);
-			
+
 			// 5. 결과 가져오기
-			while(rs.next()) {
+			while (rs.next()) {
 				// String name = rs.getString(1); // db는 1부터 시작함
 				// String owner = rs.getString(2);
 				// String birth = rs.getString(3);
-				
+
 				long no = rs.getLong("book.no");
 				String title = rs.getString("book.title");
 				String status = rs.getString("book.status");
 				String authorName = rs.getString("author.name");
-				
+
 				BookVo vo = new BookVo();
-				
+
 				vo.setNo(no);
 				vo.setTitle(title);
 				vo.setStatus(status);
 				vo.setAuthorName(authorName);
-				
+
 				list.add(vo);
 			}
-			
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		} finally {
 			try {
-				if(rs != null) {
+				if (rs != null) {
 					rs.close();
 				}
-				
-				if(stmt != null) {
+
+				if (stmt != null) {
 					rs.close();
 				}
-				
+
 				if (conn != null) {
 					conn.close();
 				}
@@ -71,23 +71,59 @@ public class BookDao {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return list;
 	}
-	
+
+	public boolean updateStatus(long no, String status) {
+		boolean result = false;
+
+		try {
+			conn = getConnection();
+
+			String sql = "update book set status='대여중' where no = ?";
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setLong(1, no);
+
+			int count = pstmt.executeUpdate();
+
+			if (count == 1) {
+				result = true;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+
+		return result;
+	}
+
 	public boolean insert(BookVo bookVo) {
 		boolean result = false;
 
 		try {
 			conn = getConnection();
-			
+
 			String sql = "insert into book values(null, ?, '대여가능', ?);";
-			
+
 			pstmt = conn.prepareStatement(sql);
 
 			pstmt.setString(1, bookVo.getTitle());
 			pstmt.setLong(2, bookVo.getAuthorNo());
-			
+
 			int count = pstmt.executeUpdate();
 
 			return count == 1;
@@ -116,12 +152,12 @@ public class BookDao {
 
 			String url = "jdbc:mysql://localhost:3306/webdb?useSSL=false";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		
+
 			System.out.println("DB 연결 성공");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		return conn;
 	}
 
