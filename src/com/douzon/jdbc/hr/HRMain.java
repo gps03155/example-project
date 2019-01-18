adbbf125f11c0167e4067e1a8e78277a36174347
박성혜 <gps03155@gmail.com>
2019-01-18 16:00:35 +0900

    실습문제1 - EmployeeDao 풀이 완료

--- src/com/douzon/jdbc/hr/HRMain.java
+++ src/com/douzon/jdbc/hr/HRMain.java
@@ -1,10 +1,28 @@
 package com.douzon.jdbc.hr;
 
+import java.util.List;
+import java.util.Scanner;
+
+import com.douzon.jdbc.hr.dao.EmployeeDao;
+import com.douzon.jdbc.hr.vo.EmployeeVo;
+
 public class HRMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("사원 이름을 입력해주세요 : ");
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.nextLine();
+		
+		System.out.println();
+		search(name);
 	}
 
+	public static void search(String name) {
+		List<EmployeeVo> list = new EmployeeDao().getList(name);
+		
+		for(EmployeeVo vo : list) {
+			System.out.println(vo.getEmp_no() + " " + vo.getName() + " " + vo.getHire_date());
+		}
+	}
 }
--- src/com/douzon/jdbc/hr/dao/EmployeeDao.java
+++ src/com/douzon/jdbc/hr/dao/EmployeeDao.java
@@ -1,5 +1,83 @@
 package com.douzon.jdbc.hr.dao;
 
-public class EmployeeDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.douzon.jdbc.hr.vo.EmployeeVo;
+
+public class EmployeeDao {
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+
+	public List<EmployeeVo> getList(String name){
+		List<EmployeeVo> list = new ArrayList<EmployeeVo>();
+		
+		try {
+		conn = getConnection();
+		
+		String sql = "select emp_no, concat(first_name, ' ', last_name), hire_date from employees where first_name Like ? or last_name Like ?";
+		
+		pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setString(1, "%" + name + "%");
+		pstmt.setString(2, "%" + name + "%");
+		
+		rs = pstmt.executeQuery();
+		
+		while(rs.next()) {
+			String emp_no = rs.getString("emp_no");
+			String fullName = rs.getString("concat(first_name, ' ', last_name)");
+			String hire_date = rs.getString("hire_date");
+			
+			EmployeeVo vo = new EmployeeVo();
+			
+			vo.setEmp_no(emp_no);
+			vo.setName(fullName);
+			vo.setHire_date(hire_date);
+			
+			list.add(vo);
+		}
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return list;
+	}
+	
+	public Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			String url = "jdbc:mysql://localhost:3306/employees?useSSL=false";
+			conn = DriverManager.getConnection(url, "hr", "hr");
+			
+			System.out.println("DB 연결 성공");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return conn;
+	}
 }
