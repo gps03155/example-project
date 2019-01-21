c4af8c163eeb8dbad3ae1f7df6384588e92e81a3
박성혜 <gps03155@gmail.com>
2019-01-21 20:52:04 +0900

    평가과제 - OrderBookVo 추가

---
+++ src/com/douzon/bookmall/vo/OrderBookVo.java
@@ -0,0 +1,31 @@
+package com.douzon.bookmall.vo;
+
+public class OrderBookVo {
+	private int no;
+	private String title;
+	private int num;
+	
+	public int getNo() {
+		return no;
+	}
+	
+	public void setNo(int no) {
+		this.no = no;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public int getNum() {
+		return num;
+	}
+	
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
