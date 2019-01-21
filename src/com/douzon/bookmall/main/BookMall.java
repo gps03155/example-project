b8d6a916d93417d8f5a32fb14f0ebe3fc5e4bf2c
박성혜 <gps03155@gmail.com>
2019-01-21 10:00:54 +0900

    first commit

---
+++ src/com/douzon/bookmall/main/BookMall.java
@@ -0,0 +1,17 @@
+package com.douzon.bookmall.main;
+
+public class BookMall {
+
+	public static void main(String[] args) {
+		// test 후 insert하는 코드들 넣기
+		// primary key = 2번부터
+		// 개별젹으로 테스트 후 db 리셋 후 지정된 결과가 나오게끔 출력
+		// 회원 : 이름, 전화번호, 이메일, 비밀번호
+		// 카테고리 : 번호, 이름
+		// 상품리스트 : 제목, 가격, 카테고리  
+	    // 카트리스트 : 회원 1명꺼만 보여주면 됨
+		// 주문 : 카트에 있는 물건 주문하기
+		// 주문 도서 리스트 출력
+		// ========== 타이틀 =========== 적어서 알아보기 쉽게 출력하기
+	}
+}
