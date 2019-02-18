a5ec834af73b43d73254517f963d75e7833e5f2d
박성혜 <gps03155@gmail.com>
2019-02-18 15:12:18 +0900

    emaillist3

---
+++ src/main/java/com/douzone/emaillist/controller/EmailListController.java
@@ -0,0 +1,26 @@
+package com.douzone.emaillist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.douzone.emaillist.dao.EmailListDao;
+import com.douzone.emaillist.vo.EmailListVo;
+
+@Controller
+public class EmailListController {
+	@Autowired
+	private EmailListDao dao;
+	
+	@ResponseBody
+	@RequestMapping("")
+	public String list() {		
+		List<EmailListVo> list = dao.getList();
+		System.out.println(list);
+		
+		return "/WEB-INF/views/list.jsp";
+	}
+}
