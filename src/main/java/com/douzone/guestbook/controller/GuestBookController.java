31aa8e24fb26008c99e01f48bca3641378efc089
박성혜 <gps03155@gmail.com>
2019-02-18 16:18:32 +0900

    과제 - GuestBook3 삽입, 삭제 기능 구현 완료

---
+++ src/main/java/com/douzone/guestbook/controller/GuestBookController.java
@@ -0,0 +1,44 @@
+package com.douzone.guestbook.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.douzone.guestbook.dao.GuestBookDao;
+import com.douzone.guestbook.vo.GuestBookVo;
+
+@Controller
+public class GuestBookController {
+	@Autowired
+	private GuestBookDao dao;
+	
+	@RequestMapping("")
+	public String index(Model model) {
+		model.addAttribute("list", dao.getList());
+		
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(GuestBookVo vo) {
+		dao.insert(vo);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/deleteform")
+	public String delete(@RequestParam(value="no", required=true, defaultValue="") Long no) {
+		
+		return "/WEB-INF/views/deleteform.jsp";
+	}
+	
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
+	public String delete(int no, String password) {
+		dao.delete(no, dao.comparePW(password));
+
+		return "redirect:/";
+	}
+}
