cdb244965110b62f5e63e15a0f85ab4930aafe27
박성혜 <gps03155@gmail.com>
2019-03-21 15:46:53 +0900

    spring java config : multipart resolver

--- src/main/java/com/douzone/mysite/aspect/GlobalExceptionHander.java
+++ src/main/java/com/douzone/mysite/aspect/GlobalExceptionHander.java
@@ -2,9 +2,11 @@ package com.douzone.mysite.aspect;
 
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
-@Aspect
+// @Aspect
 // @Component
+@ControllerAdvice
 public class GlobalExceptionHander {
 
 	@AfterThrowing(value="execution(* *..*.*.*(..))", throwing="ex") // Global Exception 처리 - 모든 패키지에서 모든 메서드가 실행할때 예외처리
--- src/main/java/com/douzone/mysite/config/WebConfig.java
+++ src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import com.douzone.config.web.FileUploadConfig;
 import com.douzone.config.web.MVCConfig;
 import com.douzone.config.web.MessagesConfig;
 import com.douzone.config.web.SecurityConfig;
 
 @Configuration
-@ComponentScan("com.douzone.mysite.controller")
-@Import(value= {MVCConfig.class, SecurityConfig.class, MessagesConfig.class})
+@ComponentScan(value= {"com.douzone.mysite.controller", "com.douzone.mysite.exception"})
+@Import(value= {MVCConfig.class, SecurityConfig.class, MessagesConfig.class, FileUploadConfig.class})
 public class WebConfig {
 	
 }
