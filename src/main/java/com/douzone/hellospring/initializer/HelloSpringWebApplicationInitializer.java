8bb653927107bf6178738505bb578e03d9192ca5
박성혜 <gps03155@gmail.com>
2019-03-22 10:10:48 +0900

    spring java config : web.xml remove

---
+++ src/main/java/com/douzone/hellospring/initializer/HelloSpringWebApplicationInitializer.java
@@ -0,0 +1,25 @@
+package com.douzone.hellospring.initializer;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.douzone.hellospring.config.WebConfig;
+
+public class HelloSpringWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	// DispatcherServlet의 url 매핑
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
