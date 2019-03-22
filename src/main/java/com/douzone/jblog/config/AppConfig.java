19ab53ea41d7321943537674bff7710d89ff9749
박성혜 <gps03155@gmail.com>
2019-03-22 12:57:53 +0900

    spring java config : DB, MyBatis

--- src/main/java/com/douzone/jblog/config/AppConfig.java
+++ src/main/java/com/douzone/jblog/config/AppConfig.java
@@ -2,8 +2,14 @@ package com.douzone.jblog.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.douzone.config.app.DBConfig;
+import com.douzone.config.app.MyBatisConfig;
 
 @Configuration
+@ComponentScan(value= {"com.douzone.jblog.repository", "com.douzone.jblog.service"})
+@Import(value= {DBConfig.class, MyBatisConfig.class})
 public class AppConfig {
 
 }
