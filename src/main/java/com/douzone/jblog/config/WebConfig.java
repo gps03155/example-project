45b7b8b4fcb5ebb8f0bfab9455e4de05d68d46ac
박성혜 <gps03155@gmail.com>
2019-03-22 12:12:12 +0900

    spring java config : messagesource

--- src/main/java/com/douzone/jblog/config/WebConfig.java
+++ src/main/java/com/douzone/jblog/config/WebConfig.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.douzone.config.web.MVCConfig;
+import com.douzone.config.web.MessagesConfig;
 
 @Configuration
 @ComponentScan(value= {"com.douzone.jblog.controller"})
-@Import(value= {MVCConfig.class})
+@Import(value= {MVCConfig.class, MessagesConfig.class})
 public class WebConfig {
 
 }
