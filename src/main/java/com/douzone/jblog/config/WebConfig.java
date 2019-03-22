90dd5082b897dfce96798c7e35262b45d8e02845
박성혜 <gps03155@gmail.com>
2019-03-22 14:04:36 +0900

    spring java config : aop-autoproxy

--- src/main/java/com/douzone/jblog/config/WebConfig.java
+++ src/main/java/com/douzone/jblog/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.douzone.jblog.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 import com.douzone.config.web.FileUploadConfig;
@@ -10,6 +11,7 @@ import com.douzone.config.web.MessagesConfig;
 import com.douzone.config.web.SecurityConfig;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan(value= {"com.douzone.jblog.controller"})
 @Import(value= {MVCConfig.class, MessagesConfig.class, FileUploadConfig.class, SecurityConfig.class})
 public class WebConfig {
