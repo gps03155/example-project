0d4a1ab2d1e55a16cb06f1d430b1c43ab4dc3b11
박성혜 <gps03155@gmail.com>
2019-03-26 08:47:28 +0900

    spring boot : aop

---
+++ spring-boot-mysite/src/main/java/com/douzone/mysite/aspect/MeasureExecutionTimeAspect.java
@@ -0,0 +1,38 @@
+package com.douzone.mysite.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class MeasureExecutionTimeAspect {
+	
+	@Around("execution(* *..repository.*.*(..)) || execution(* *..service.*.*(..)) || execution(* *..controller.*.*(..))") // com.douzone.repository의 모든 클래스의 모든 메서드	
+	public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("------------> AOP start");
+		
+		// before
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		
+		// method 실행
+		Object result = pjp.proceed();
+		
+		// after
+		stopWatch.stop();
+		Long totalTime = stopWatch.getTotalTimeMillis();
+		
+		// System.out.println("TotalTime : " + totalTime); // 한번 로그인 이후 캐시 때문에 시간 단축
+		
+		String className = pjp.getTarget().getClass().getName(); // target 객체의 getClass - class 정보
+		String methodName = pjp.getSignature().getName(); // method 이름, 파라미터 정보
+		String taskName = className + "." + methodName;
+		
+		System.out.println("[ExecutionTime][" + taskName + "] " + totalTime + "millis");
+		
+		return result;
+	}
+}
